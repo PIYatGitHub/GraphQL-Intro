0a55891b02ead484b14fe6c97adb615a1a499303
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-07-12 18:36:42 +0300

    adding mutiple selects for users and posts...

--- src/index.js
+++ src/index.js
@@ -1,13 +1,40 @@
 import {GraphQLServer} from 'graphql-yoga'
 
+const user_seed = [
+  {
+    id: "99100101",
+    name: "Pesho",
+    email: "pesho@example.com"
+  },
+  {
+    id: "100200300",
+    name: "Yoyo",
+    email: "yoyo@example.com"
+  }
+];
+
+const posts_seed = [
+  {
+    id: "post112314",
+    title: "New post",
+    body: "sample post body...",
+    published: false
+  },
+  {
+    id: "post1352",
+    title: "GraphQL - in details",
+    body: "Graph Ql is suupa cool - use it...",
+    published: true
+  }
+];
+
 //Type definitions, e.g. the app schema
 const typeDefs = `
   type Query {
-   greeting(name: String): String!
    me: User!
    post: Post!
-   grades: [Int!]!
-   add (numbers: [Float!]!): Float!
+   users(query: String): [User!]!
+   posts(query: String): [Post!]!
   }
   type User {
     id: ID!
@@ -26,31 +53,20 @@ const typeDefs = `
 //API Resolvers
 const resolvers = {
   Query:{
-    greeting (parent, args, ctx, info){
-      if (args.name) return `Hello, ${args.name}!`;
-      return `Hello!`
-    },
-    grades (parent, args, ctx, info){
-      return [99, 25, 80, 73, 16, 100]
-    },
-    add (parent, args, ctx, info) {
-      if (!args.numbers.length) return 0;
-      return args.numbers.reduce((acc, currVal) =>acc + currVal);
-    },
     me (parent, args, ctx, info){
-      return {
-        id: "123abv",
-        name: "Pesho",
-        email: "pesho@example.com"
-      }
+      return user_seed[0]
     },
     post (parent, args, ctx, info){
-      return {
-        id: "post112314",
-        title: "New post",
-        body: "sample post body...",
-        published: false
-      }
+      return posts_seed[0]
+    },
+    users(parent, args, ctx, info){
+      if (args.query) return user_seed.filter((user)=> user.name.toLowerCase().includes(args.query.toLowerCase()));
+      return user_seed
+    },
+    posts(parent, args, ctx, info){
+      if (args.query) return posts_seed.filter((post)=> post.title.toLowerCase().includes(args.query.toLowerCase()) ||
+        post.body.toLowerCase().includes(args.query.toLowerCase()));
+      return posts_seed
     }
   }
 };
