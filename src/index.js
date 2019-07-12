6344658315b86206227d1c4d5fa32e19bd875891
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-07-12 10:00:57 +0300

    imports and exports -- basics with strings

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log("Hi graphQL!");
+import myCurrentLocation,{secret, name} from "./myModule";
+
+
+console.log("Here is a top secret..." + secret);
+console.log("The name " + name);
+console.log("From location " + myCurrentLocation);
---
+++ src/myModule.js
@@ -0,0 +1,5 @@
+const secret = "...>top secret message here<<<...",
+      name = "Peterr Iv. Yonkov",
+      location = "Sofia";
+
+export {secret, name, location as default}
