f0d6383c8957bd7eb36122865f5f7c327d3821d5
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-07-12 10:48:15 +0300

    very basic setup of the graphQL server

--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
-import myCurrentLocation,{secret, name, getGreeting} from "./myModule";
-import add, {sub} from "./math";
+import {GraphQLServer} from 'graphql-yoga'
 
-console.log(getGreeting('Jessie'));
-console.log("----------MATH.JS-------------");
-console.log(add(1,2));
-console.log(sub(10, 18));
-console.log("----------MATH.JS-------------");
+//Type definitions, e.g. the app schema
+const typeDefs = `
+  type Query {
+    hello: String!
+    name: String!
+    location: String!
+    bio: String!
+  }
+`;
 
+//API Resolvers
+const resolvers = {
+  Query:{
+    hello(){
+      return 'This is the response to hello!'
+    },
+    name(){
+      return 'Peter Iv. Yonkov'
+    },
+    location(){
+      return 'Sofia, Bulgaria'
+    },
+    bio(){
+      return 'A non-robot dev, who is working very hard on new skills!'
+    }
+  }
+};
 
+const server = new GraphQLServer({
+  typeDefs, resolvers
+});
 
+server.start(()=>{
+  console.log('the server is up!');
+});
--- src/myModule.js
+++
@@ -1,7 +0,0 @@
-const secret = "...>top secret message here<<<...",
-      name = "Peter Iv. Yonkov",
-      location = "Sofia";
-const getGreeting = (name) => {
-  return `Greetings, ${name}!`
-}
-export {secret, name, getGreeting, location as default}
